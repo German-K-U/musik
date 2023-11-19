fab2089d93b651cbe381981259d7a6c161cdd470
DESKTOP-E0EPL3E\DEV <groof6@gmail.com>
2023-11-18 17:21:31 -0800

    Input

--- src/components/Treck/Track.jsx
+++ src/components/Treck/Track.jsx
@@ -2,9 +2,12 @@
 import style from "./track.module.scss";
 import { IconButton} from "@mui/material";
 import { PlayArrow } from "@mui/icons-material";
+import secondsToMMSS from "../../utils/secondsToMMSS";
 
 const  Track = (track) => {
     const { id, src, preview, title, artists, duration} = track;
+
+    const formattedDuration = secondsToMMSS(duration);
 
     return (
     <div className={style.track}>
@@ -16,7 +19,7 @@ const  Track = (track) => {
             <b>{title}</b>
             <p>{artists}</p>
         </div>
-        <p>{duration}</p>
+        <p>{formattedDuration}</p>
     </div>
 
     );
