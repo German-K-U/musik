f5fbe71b05007335d8b778a43061e2feb9d703a6
DESKTOP-E0EPL3E\DEV <groof6@gmail.com>
2023-11-18 17:05:38 -0800

    go go go

--- src/components/Treck/Track.jsx
+++ src/components/Treck/Track.jsx
@@ -1,7 +1,25 @@
+
+import style from "./track.module.scss";
+import { IconButton} from "@mui/material";
+import { PlayArrow } from "@mui/icons-material";
+
 const  Track = (track) => {
     const { id, src, preview, title, artists, duration} = track;
 
-    return <div>{JSON.stringify(track)}</div>
+    return (
+    <div className={style.track}>
+        <IconButton>
+            <PlayArrow/>
+        </IconButton>
+        <img className={style.preview} src={preview} alt = "" />  
+        <div className={style.credits}>
+            <b>{title}</b>
+            <p>{artists}</p>
+        </div>
+        <p>{duration}</p>
+    </div>
+
+    );
 };
 
 export default Track;
