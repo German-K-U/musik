2f58b05aebc2cf011883f72997721d90eca1bd2a
GermanKaskov <groof6@gmail.com>
2023-12-04 13:54:16 +0300

    job.fine

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,20 @@
 import tracksList from "../../assets/tracksList";
 import style from "./mainPage.module.scss";
 import Track from "../../components/Treck/Track";
+import {Input} from "@mui/material";
 
 const MainPage = () => {
+        const handleChange = (event) => {
+            console.log(event.target.value)
+        } 
+
     return (
-     <div className={style.search}>
-        <>Поиск треков</>
+     <div className={style.search} >
+        <Input 
+             className={style.input} 
+             placeholder="Поиск треков" 
+             onChange={handleChange} 
+        />
         <div className={style.list}>
             {tracksList.map((track) =>(
                <Track key = {track.id}  {...track} />
