70f257cd1e6bf7ac92131cf6f65c104883c5b3cc
GermanKaskov <groof6@gmail.com>
2023-12-04 14:42:31 +0300

    errr

--- src/components/Treck/Track.jsx
+++ src/components/Treck/Track.jsx
@@ -1,4 +1,6 @@
 
+import { useContext } from "react";
+import { AudioContext } from "../../context/AudioContext";
 import style from "./track.module.scss";
 import { IconButton} from "@mui/material";
 import { PlayArrow } from "@mui/icons-material";
@@ -7,11 +9,13 @@ import secondsToMMSS from "../../utils/secondsToMMSS";
 const  Track = (track) => {
     const { id, src, preview, title, artists, duration} = track;
 
+    const { handleToggleAudio } = useContext(AudioContext)
+    
     const formattedDuration = secondsToMMSS(duration);
 
     return (
     <div className={style.track}>
-        <IconButton>
+        <IconButton onClick={()=> handleToggleAudio(track)}>
             <PlayArrow/>
         </IconButton>
         <img className={style.preview} src={preview} alt = "" />  
